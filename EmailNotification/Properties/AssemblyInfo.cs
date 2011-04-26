23a8aec7af6c39314649419615ebc96723f6dc58
Mark Jourdan <mjourdan@markjourdan.com>
2011-04-26 03:43:03 +0000

    Pushed a new release with the new changes

--- EmailNotification/Properties/AssemblyInfo.cs
+++ EmailNotification/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1115.22")]
-[assembly: AssemblyFileVersion("0.1.1116.2159")]
+[assembly: AssemblyVersion("0.1.1115.25")]
+[assembly: AssemblyFileVersion("0.1.1115.25")]
