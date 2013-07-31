306084ee34ff1ec0b3e9e5c6355499c1b56440d3
mjourdan_cp <SND\mjourdan_cp>
2013-07-31 22:30:38 +0000

    versions have been updated and some modifications to the build

--- EmailNotification/Properties/AssemblyInfo.cs
+++ EmailNotification/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mark Jourdan")]
 [assembly: AssemblyProduct("EmailNotification")]
-[assembly: AssemblyCopyright("Mark Jourdan - 2011")]
+[assembly: AssemblyCopyright("Mark Jourdan - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1298.29")]
-[assembly: AssemblyFileVersion("0.1.1298.29")]
+[assembly: AssemblyVersion("0.2.3212.30")]
+[assembly: AssemblyFileVersion("0.2.3212.30")]
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 
-nant -buildfile:_Build\emailNotification.build -D:build.configuration=Release
+nant -buildfile:_Build\emailNotification.build -D:build.configuration=Release -D:version.number=0.2.0.0
 
 
 pause
