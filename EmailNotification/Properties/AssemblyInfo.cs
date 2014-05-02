e3d9694c0b5c8c7f64ce01cc41e06ff5a5f138f3
mjourdan_cp <SND\mjourdan_cp>
2014-05-02 22:31:08 +0000

    set the assembly version back to 1.0.0.0 (let TeamCity handle versioning) Set the Copyright to 2014

--- EmailNotification/Properties/AssemblyInfo.cs
+++ EmailNotification/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mark Jourdan")]
 [assembly: AssemblyProduct("EmailNotification")]
-[assembly: AssemblyCopyright("Mark Jourdan - 2013")]
+[assembly: AssemblyCopyright("Mark Jourdan - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.3213.31")]
-[assembly: AssemblyFileVersion("0.2.3213.31")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
