ea32d8693818c1144de2e5e3db32e269f14d1ab9
mjourdan_cp <SND\mjourdan_cp>
2014-05-02 23:28:23 +0000

    changed to use nuget

--- EmailNotification.Tests/Properties/AssemblyInfo.cs
+++ EmailNotification.Tests/Properties/AssemblyInfo.cs
@@ -21,15 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a9891d2b-2173-4c12-9488-e68d4b10439f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
