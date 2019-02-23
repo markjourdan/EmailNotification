9dc9599a2f129f4da7d3b33c5f8001aaf31ad8e9
Mark Jourdan <mjourdan@markjourdan.com>
2019-02-23 12:15:57 -0600

    updated the name of the nuspec

--- build.ps1
+++ build.ps1
@@ -1,8 +1,10 @@
 ﻿
-$msbuild = "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe"
+nuget.ext
 
-$buildOptions = "/t:rebuild /p:Configuration=Release /p:VersionNumber=0.3.0.0"
+$msbuild = "$Env:Programfiles (x86)\Microsoft Visual Studio\2017\buildtools\msbuild\15.0\Bin\MSbuild.exe"
+$solution = $PSScriptRoot + "\EmailNotification.sln"
 
-Invoke-Expression $msbuild EmailNotification.sln 
+
+& $msbuild $solution /t:rebuild /p:Configuration=Release /p:VersionNumber=0.3.0.0
 
 
