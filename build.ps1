2927b6b07f43dec87fd72125920b64257e52ebba
Mark Jourdan <mjourdan@markjourdan.com>
2019-02-23 09:53:21 -0600

    removed unnecssary files

---
+++ build.ps1
@@ -0,0 +1,8 @@
+﻿
+$msbuild = "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe"
+
+$buildOptions = "/t:rebuild /p:Configuration=Release /p:VersionNumber=0.3.0.0"
+
+Invoke-Expression $msbuild EmailNotification.sln 
+
+
