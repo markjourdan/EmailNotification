d4418d0170eb88c383cb5b3eca2619b306c8cf8a
Mark Jourdan <mjourdan@markjourdan.com>
2019-02-25 13:09:02 -0600

    removed the bat nuget publish, no longer need

--- _Build/publish nuget.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-@echo SET /p ver= What is the version number?
-
-SET mypath = %cd%
-
-@echo EmailNotification\nuget.exe pack -Version %ver% -symbols EmailNotification\EmailNotification.csproj
-@echo EmailNotification\nuget.exe push .\EmailNotification.%ver%.nupkg
-pause
