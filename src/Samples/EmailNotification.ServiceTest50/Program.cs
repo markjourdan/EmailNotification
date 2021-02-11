eff5c8df50d8be4698500c29bf52ed8a8f44779b
Mark Jourdan <mjourdan@markjourdan.com>
2021-02-11 16:26:54 -0600

    Added a .NET 5.0 Sample Updated a few of the dependencies

---
+++ src/Samples/EmailNotification.ServiceTest50/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using EmailNotification.Core;
+
+namespace EmailNotification.ServiceTest50
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var emails = new List<MessageQueueEntity>
+            {
+                new MessageQueueEntity
+                {
+                    To = "test@test.com",
+                    From = "from@test.com",
+                    Body = "Test Email",
+                    BodyFormat = BodyFormat.PlainText,
+                    Created = DateTime.Now
+                }
+            };
+
+
+            var configuration = Master.UseAppConfig().WithEmails(emails);
+
+            Master.Execute(configuration);
+        }
+    }
+}
